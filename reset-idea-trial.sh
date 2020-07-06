30122130aad5bfa0b15d6c7851560aa80c1b1192
Kok Kek <kokkekpek@google.com>
2020-07-06 04:47:48 +0300

    Remove exit

--- reset-idea-trial.sh
+++ reset-idea-trial.sh
@@ -3,4 +3,3 @@ rm -r ~/.config/JetBrains/IntelliJIdea2020.1/eval/idea201.evaluation.key
 sed -i '/evlsprt/d' ~/.config/JetBrains/IntelliJIdea2020.1/options/other.xml 
 sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
 rm -rf ~/.java/.userPrefs/jetbrains/idea/*
-exit
